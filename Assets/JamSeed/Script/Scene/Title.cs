f52a5cda868a734d7ddacf4a7cfc9f213bdde931
Liuisei <a0966891512@gmail.com>
2025-08-03 04:26:48 +0900

    tween

--- Assets/JamSeed/Script/Scene/Title.cs
+++ Assets/JamSeed/Script/Scene/Title.cs
@@ -1,33 +1,39 @@
 using JamSeed.Foundation;
 using UnityEngine;
+using DG.Tweening; // DOTween
 
 public class Title : SceneSingleton<Title>
 {
     [SerializeField] LiteButton startButton;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // アニメーション時間
+    [SerializeField] float hoverScaleDuration = 0.2f;
+    [SerializeField] float normalScaleDuration = 0.2f;
+
+    // スケール値
+    [SerializeField] float hoverScale = 1.2f;
+
     void Start()
     {
-
         startButton.AddOnClick(() =>
         {
             //SceneManager.LoadScene("Game");
         });
+
         startButton.AddOnEnter(() =>
         {
-            startButton.transform.localScale = Vector3.one * 1.2f;
+            startButton.transform
+                .DOScale(Vector3.one * hoverScale, hoverScaleDuration)
+                .SetEase(Ease.OutBack);
             Debug.Log("Start Button Hovered");
         });
+
         startButton.AddOnExit(() =>
         {
-            startButton.transform.localScale = Vector3.one;
+            startButton.transform
+                .DOScale(Vector3.one, normalScaleDuration)
+                .SetEase(Ease.OutBack);
             Debug.Log("Start Button Exited");
         });
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
